50b868a2f81479c0d609915d40c9856bb10874c6
Alejandro <aramirez@musicbattles.mx>
2020-09-22 13:29:57 -0500

    Package.swift updated

--- Package.swift
+++ Package.swift
@@ -22,8 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tweener",
-            path: "Source",
-            dependencies: []),
+            path: "Source"),
         .testTarget(
             name: "TweenerTests",
             dependencies: ["Tweener"],
